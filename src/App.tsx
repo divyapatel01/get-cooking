b462afc2afe8c46b76191466c7e95c2de09aa0d9
Divya Patel <divyapatel1697@gmail.com>
2024-05-13 17:01:07 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src="/logo.png" className="app-logo" alt="logo" />
+        <p>Coming Soon</p>
+      </header>
+    </div>
+  );
+}
